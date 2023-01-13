1ed920b6aff1166ce1a5c0c0fd7d05c9124026c7
Grace Yuan <gyuan@nsf.gov>
2023-01-13 17:51:00 -0500

    Add author and last update date

--- bikeshare.py
+++ bikeshare.py
@@ -2,6 +2,10 @@ import time
 import pandas as pd
 import numpy as np
 
+''' Author: Grace Yuan
+    Last Update Date: 01/13/2023
+'''
+
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
